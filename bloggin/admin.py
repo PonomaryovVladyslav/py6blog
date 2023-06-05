a7eed512c462887c0a920172e42d1d5497ef2602
MykolaShv <113477121+MykolaShv@users.noreply.github.com>
2023-06-05 15:31:00 +0300

    Add files via upload

--- bloggin/admin.py
+++ bloggin/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from .models import Author, Blogpost, Comment, Topic
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ('name', 'date_of_registration')
+
+    def create_date(self, obj):
+        return obj.created
+
+    create_date.empty_value_display = '???'
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    fields = ('titleTopic', 'text', 'date_of_public', 'authors')
+
+    def create_date(self, obj):
+        return obj.created
+
+    create_date.empty_value_display = '???'
+
+
+@admin.register(Blogpost)
+class BlogpostAdmin(admin.ModelAdmin):
+    fields = ('titleBlogpost', 'text', 'date_of_public', 'author', 'topics')
+
+    def create_date(self, obj):
+        return obj.created
+
+    create_date.empty_value_display = '???'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('text', 'date_of_public', 'author', 'blogpost')
+
+    def create_date(self, obj):
+        return obj.created
+
+    create_date.empty_value_display = '???'
+
+
 
 # Register your models here.
--- bloggin/models.py
+++ bloggin/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+    date_of_registration = models.DateTimeField()
+
+
+class Topic(models.Model):
+    titleTopic = models.CharField(max_length=128)
+    text = models.TextField(null=True, blank=True)
+    date_of_public = models.DateTimeField()
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.titleTopic
+
+
+class Blogpost(models.Model):
+    titleBlogpost = models.CharField(max_length=120)
+    text = models.TextField(null=True, blank=True)
+    date_of_public = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='blogpost')
+    topics = models.ManyToManyField(Topic)
+
+    def __str__(self):
+        return self.titleBlogpost
+
+
+class Comment(models.Model):
+    text = models.TextField(null=True, blank=True)
+    date_of_public = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='author')
+    blogpost = models.ForeignKey(Blogpost, on_delete=models.CASCADE, related_name='blogpost')
+
+
+
+
 # Create your models here.
