56f0bf18f816032724bc31a9bc815191e7eeb280
Vlad Ponomarov <vladyslav.ponomarov@workday.com>
2023-08-07 19:32:13 +0200

    add settings_prod.py

---
+++ myblog/settings_prod.py
@@ -0,0 +1,17 @@
+import os
+
+DEBUG = False
+ALLOWED_HOSTS = ['3.68.80.16']
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DBNAME'),
+        'USER': os.environ.get('DBUSER'),
+        'PASSWORD': os.environ.get('DBPASS'),
+        'HOST': os.environ.get('DBHOST', '127.0.0.1'),
+        'PORT': os.environ.get('DBPORT', '5432'),
+    }
+}
